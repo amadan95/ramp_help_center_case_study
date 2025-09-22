f426091a5682abe7f4fd209a2e2bf6341b25b591
Alok Madan <amadan656@gmail.com>
2025-09-22 10:54:18 -0400

    build(vercel): stub expo-router SSR entry via metro alias to avoid export errors

---
+++ help-center-app/stubs/expo-router-render.js
@@ -0,0 +1,7 @@
+module.exports = async function render() {
+	return {
+		html: '<!doctype html><html><head><meta charset="utf-8"/></head><body><div id="root"></div></body></html>',
+		head: [],
+		resources: []
+	};
+};
